f4f25a130420aba4cfaaca0fbcf3cd38b709b6d2
Dhia Tr <trabelsidhia19@gmail.com>
2020-06-01 15:58:11 +0100

    agent addition with validation

--- src/app/agent-form/agent-form.component.ts
+++ src/app/agent-form/agent-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseService } from '../services/base.service';
 import { AgentService } from '../services/agent.service';
+import { InvalidData } from '../common/invalid-data';
+import { BaseNotExistant } from '../common/base-not-existant';
 
 @Component({
   selector: 'app-agent-form',
@@ -10,6 +12,8 @@ import { AgentService } from '../services/agent.service';
 export class AgentFormComponent implements OnInit {
 
   bases;
+  Added = false;
+
   constructor(
     private baseService: BaseService,
     private agentService: AgentService
@@ -22,9 +26,20 @@ export class AgentFormComponent implements OnInit {
   }
 
   submit(form) {
-    let err;
-    this.agentService.addOne(form.value).subscribe(result => console.log(result), err => { console.log('err'); console.log(err); });
-    // console.log(form.value);
+    this.agentService.addOne(form.value).subscribe(result => {
+      this.Added = true;
+    },
+      err => {
+        if (err instanceof InvalidData)
+          form.form.setErrors({ invalidData: true });
+        else
+          form.form.setErrors({ unknownError: true });
+      });
   }
+
+  DismissSuccessMessage() {
+    this.Added = false;
+  }
+
 
 }
--- src/app/services/agent.service.ts
+++ src/app/services/agent.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { InvalidData } from '../common/invalid-data';
+import { BaseNotExistant } from '../common/base-not-existant';
+import { AppError } from '../common/app-error';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +14,20 @@ export class AgentService {
   constructor(private http: HttpClient) { }
 
   addOne(agentData) {
-    return this.http.post('http://localhost:3000/api/agents/', agentData, this.getHeader()).pipe(catchError(err => throwError(err)));
+    return this.http.post('http://localhost:3000/api/agents/', agentData, this.getHeader())
+      .pipe(catchError(this.handleError));
   }
 
   getHeader() {
     const token = localStorage.getItem('token');
     return { headers: { 'x-auth-token': token } };
+  }
+
+  handleError(error) {
+    if (error.status === 400)
+      return throwError(new InvalidData(error));
+    else if (error.status === 404)
+      return throwError(new BaseNotExistant(error));
+    return throwError(new AppError(error));
   }
 }
